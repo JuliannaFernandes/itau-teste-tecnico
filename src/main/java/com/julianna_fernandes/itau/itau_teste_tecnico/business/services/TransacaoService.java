f2ce070b8ab054862de05a2d7a9e2c065f692263
Julianna Fernandes <juliannabf29@gmail.com>
2025-02-03 22:20:01 -0300

    feat: criando método de adicionar transações

--- src/main/java/com/julianna_fernandes/itau/itau_teste_tecnico/business/services/TransacaoService.java
+++ src/main/java/com/julianna_fernandes/itau/itau_teste_tecnico/business/services/TransacaoService.java
@@ -1,11 +1,33 @@
 package com.julianna_fernandes.itau.itau_teste_tecnico.business.services;
 
+import com.julianna_fernandes.itau.itau_teste_tecnico.controller.dtos.TransacaoRequestDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class TransacaoService {
+    private final List<TransacaoRequestDTO> listaTransacoes = new ArrayList<>();
+
+    public void adicionarTransacao(TransacaoRequestDTO dto) {
+
+        log.info("Iniciado o processamento de gravar transações");
+        if (dto.dataHora().isAfter(OffsetDateTime.now())){
+            log.info("Data de hora maiores que a data e hora atual");
+            throw new UnprocessableEntity("Data e hora maiores que a data e hora atual");
+        }
+        if(dto.valor() < 0 ){
+            log.info("Valor não pode ser menor que 0");
+            throw new UnprocessableEntity("Valor não pode ser menor que 0");
+        }
+
+        listaTransacoes.add(dto);
+    }
+
 }
--- src/main/java/com/julianna_fernandes/itau/itau_teste_tecnico/controller/dtos/TransacaoRequestDTO.java
+++ src/main/java/com/julianna_fernandes/itau/itau_teste_tecnico/controller/dtos/TransacaoRequestDTO.java
@@ -1,4 +1,6 @@
 package com.julianna_fernandes.itau.itau_teste_tecnico.controller.dtos;
 
-public record TransacaoRequestDTO() {
+import java.time.OffsetDateTime;
+
+public record TransacaoRequestDTO(Double valor, OffsetDateTime dataHora) {
 }
