b99053ad98f67590ea84d5c2f2c15fe17868cd49
Julianna Fernandes <juliannabf29@gmail.com>
2025-02-03 22:45:14 -0300

    feat: criando método de buscar transações

--- src/main/java/com/julianna_fernandes/itau/itau_teste_tecnico/business/services/TransacaoService.java
+++ src/main/java/com/julianna_fernandes/itau/itau_teste_tecnico/business/services/TransacaoService.java
@@ -17,12 +17,13 @@ public class TransacaoService {
     private final List<TransacaoRequestDTO> listaTransacoes = new ArrayList<>();
 
     public void adicionarTransacao(TransacaoRequestDTO dto) {
-
         log.info("Iniciado o processamento de gravar transações");
+
         if (dto.dataHora().isAfter(OffsetDateTime.now())){
             log.info("Data de hora maiores que a data e hora atual");
             throw new UnprocessableEntity("Data e hora maiores que a data e hora atual");
         }
+
         if(dto.valor() < 0 ){
             log.info("Valor não pode ser menor que 0");
             throw new UnprocessableEntity("Valor não pode ser menor que 0");
@@ -33,5 +34,14 @@ public class TransacaoService {
 
     public void limparTransacoes() {
         listaTransacoes.clear();
+    }
+
+    public List<TransacaoRequestDTO> buscarTransacoes(Integer intervaloBusca){
+        OffsetDateTime dataHoraIntervalo = OffsetDateTime.now().minusSeconds(intervaloBusca);
+
+        return listaTransacoes.stream()
+                .filter(transacao -> transacao.dataHora()
+                            .isAfter(dataHoraIntervalo)).toList();
+
     }
 }
